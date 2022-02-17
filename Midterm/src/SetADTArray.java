dd4f45cd1859b241c7114f53f3b3a7ea1173f6ca
rayyanharoon <56239236+rayyanharoon@users.noreply.github.com>
2022-02-17 14:45:57 -0800

    Add files via upload

--- Midterm/src/SetADTArray.java
+++ Midterm/src/SetADTArray.java
@@ -1,11 +1,13 @@
 import java.util.*;
 public class SetADTArray {
     String [] arr = new String[10];
-    int size = 0;
+    // setting the array size
+    int size = 1;
     void add(String a) {
         arr[size + 1] = a;
         size++;
     }
+    // add method
     void add(int position, String a) {
         if(position < size) {
             for(int i = size; i >= position; i--) {
@@ -15,6 +17,7 @@ public class SetADTArray {
         arr[position] = a;
         size++;
     }
+    // remove method
     void remove(int position) {
         if(position < size) {
             for(int i = position; i < size; i++) {
@@ -23,12 +26,34 @@ public class SetADTArray {
         }
         size++;
     }
+    // get + getSize methods
     String get(int position) {
         return arr[position];
     }
     int getSize() {
         return size;
     }
+    /* attempting to implement exception code
+    public void push(T a) throws SetADTException {
+        if(size == 5 ){
+            throw new SetADTException("stack full");
+        }
+        top ++;
+        arr[top] = a;
+        size ++;
+    }
+    public T pop() throws EmptySetADTException {
+        if(size == 0)
+            throw new EmptySetADTException();
+        T temp = arr[top];
+        top--;
+        size--;
+        return temp;
+    }
+    public T peek(){
+        return arr[top];
+    }
+    */
     public  static void main(String[] args)
     {
         // hash set no duplicates printed
@@ -43,7 +68,7 @@ public class SetADTArray {
         obj.remove("1");
         System.out.println(obj);
         System.out.println("checking if set is empty: " + obj.isEmpty());
-        System.out.println("clearing set . . .");
+        System.out.println("clearing set...");
         obj.clear();
         System.out.println("checking if set is empty: " + obj.isEmpty());
     }
